4da2093c6686bb761d3b152163564b0a00af93ea
Andrii Lysenko <andrewkvlp@gmail.com>
2020-02-26 14:49:31 +0200

    Change bspwm border colors, add logging for polybar.

--- bspwm/.config/polybar/launch.sh
+++ bspwm/.config/polybar/launch.sh
@@ -6,8 +6,8 @@ while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
 if type "xrandr"; then
   for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
-    MONITOR=$m polybar --reload main &
+    MONITOR=$m polybar --reload main > /tmp/polybar-$m.log 2>&1 &
   done
 else
-  polybar --reload main &
+  polybar --reload main > /tmp/polybar.log 2>&1 &
 fi
