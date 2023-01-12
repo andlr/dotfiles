b60bd46a7997d66454c424b5a11114bbd126d663
Andrii Lysenko <andrewkvlp@gmail.com>
2023-01-12 17:19:58 +0200

    Update zsh & kitty configs

--- bspwm/.config/polybar/launch.sh
+++ bspwm/.config/polybar/launch.sh
@@ -7,6 +7,9 @@ while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 if [ "$(hostname)" = 'ryzen-pc' ]; then
     MONITOR=DP-0 polybar --reload main > /tmp/polybar-main.log 2>&1 &
     MONITOR=DP-4 polybar --reload satellite > /tmp/polybar-satellite.log 2>&1 &
+elif [ "$(hostname)" = 'al-thinkpad' ]; then
+    MONITOR=eDP polybar --reload main > /tmp/polybar-main.log 2>&1 &
+    MONITOR=DisplayPort-3 polybar --reload satellite > /tmp/polybar-satellite.log 2>&1 &
 elif type "xrandr"; then
   for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
     MONITOR=$m polybar --reload main > /tmp/polybar-$m.log 2>&1 &
--- install.sh
+++ install.sh
@@ -38,8 +38,8 @@ chk() {
 
 forZsh() { 
   command -v zsh >/dev/null 2>&1 || { echo "Z shell is not installed"; exit 1; }
-  chk https://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh
-  chk https://github.com/bhilburn/powerlevel9k.git ~/.oh-my-zsh/custom/themes/powerlevel9k
+  chk https://github.com/robbyrussell/oh-my-zsh.git $HOME/.oh-my-zsh
+  chk https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
   chk https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 }
 
