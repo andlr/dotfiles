307d39e079bc0a6284212b049c957739a3205e1d
Andrii Lysenko <andrewkvlp@gmail.com>
2024-07-30 20:52:29 +0300

    Multi-monitor setup

--- bspwm/.config/polybar/launch.sh
+++ bspwm/.config/polybar/launch.sh
@@ -9,16 +9,10 @@ then
     source ~/.host.sh
 fi
 
-if [ "$(hostname)" = 'ryzen-pc' ]; then
-    MONITOR=DP-0 polybar --reload main > /tmp/polybar-main.log 2>&1 &
-    MONITOR=DP-4 polybar --reload satellite > /tmp/polybar-satellite.log 2>&1 &
-elif [ "$(hostname)" = 'al-thinkpad' ]; then
-    MONITOR=eDP polybar --reload main > /tmp/polybar-main.log 2>&1 &
-    MONITOR=DisplayPort-3 polybar --reload satellite > /tmp/polybar-satellite.log 2>&1 &
-elif type "xrandr"; then
-  for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
-    MONITOR=$m polybar --reload main > /tmp/polybar-$m.log 2>&1 &
-  done
-else
-  polybar --reload main > /tmp/polybar.log 2>&1 &
-fi
+primary_monitor=$(xrandr --query | grep "primary" | cut -d" " -f1)
+MONITOR=$primary_monitor polybar --reload main > /tmp/polybar-main.log 2>&1 &
+
+for m in $(xrandr --query | grep " connected" | grep -v "primary" | cut -d" " -f1); do
+    MONITOR=$m polybar --reload satellite > /tmp/polybar-sattelite-$m.log 2>&1 &
+done
+
